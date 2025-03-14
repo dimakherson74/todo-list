e9d161b084d6dc7736bc086fecceeec76fa7bcc3
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-14 18:26:33 +0100

    add to todo_app/view.py classes TaskCreate TaskUpdate and TaskDelete

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from todo_app.models import Task
+
+
+class TaskListView(ListView):
+    model = Task
+
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:todo-list")
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:todo-list")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
