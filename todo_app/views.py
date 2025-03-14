504ee582cb644973e438b63782e58100b3d6b6bf
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-14 19:55:34 +0100

    add to todo_app/views.py classe Tag Create, Update, List, Delete

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-from todo_app.models import Task
+from todo_app.models import Task, Tag
 
 
 class TaskListView(ListView):
@@ -24,3 +24,24 @@ class TaskUpdateView(UpdateView):
 
 class TaskDeleteView(DeleteView):
     model = Task
+
+
+class TagListView(ListView):
+    model = Tag
+
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_app:tag-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
